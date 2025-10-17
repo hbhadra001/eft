f8792829dfc128b6f1d337b6a95c9aa8b9bd334c
hbhadra001 <hbhadra001@gmail.com>
2025-10-16 18:59:34 -0700

    Update uu.sh

--- uu.sh
+++ uu.sh
@@ -1,3 +1,15 @@
 mkdir -p layer/python
 pip install -t layer/python --only-binary=:all: paramiko cryptography bcrypt pynacl
 (cd layer && zip -r ../paramiko-layer.zip python)
+
+# If you haven't built it yet:
+mkdir -p layer/python
+pip install -t layer/python --only-binary=:all: paramiko cryptography bcrypt pynacl
+( cd layer && zip -r ../paramiko-layer.zip python )
+
+# Publish the layer (replace REGION if needed)
+aws lambda publish-layer-version \
+  --layer-name paramiko-al2023 \
+  --zip-file fileb://paramiko-layer.zip \
+  --compatible-runtimes python3.12
+
